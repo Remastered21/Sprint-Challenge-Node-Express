f37a2e993d1c1ab0ceac5d32e285e0ae7a5d854f
Henry K <mykim1891@hotmail.com>
2018-05-04 08:58:48 -0700

    All of CRUDs set for project Routes; working on debug

--- router/projectRoutes.js
+++ router/projectRoutes.js
@@ -47,7 +47,48 @@ router.get("/:id/actions", (req, res) => {
       res.status(500).json({ error: err });
     });
 });
+router.post("/", (req, res) => {
+  const newProject = req.body;
+  db
+    .insert(newProject)
+    .then(response => {
+      res.status(201).json(response, newProject);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Could not save new project to database" });
+    });
+});
 
-router.post('/')
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.get(id).then(response => {
+    let deletedProj = { ...response };
+    db
+      .remove(id)
+      .then(response => {
+        res.status(200).json(deletedProj);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedProj = req.body;
+
+  db
+    .update(id, updatedProj)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
 
 module.exports = router;
