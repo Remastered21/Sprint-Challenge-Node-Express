97d4752a95eee9b5caeff02547372f8336125f75
Henry K <mykim1891@hotmail.com>
2018-05-04 15:09:12 -0700

    Basic css added to actions

--- client/src/component/Projects/ProjectDetail.js
+++ client/src/component/Projects/ProjectDetail.js
@@ -31,18 +31,17 @@ export default class ProjectDetail extends Component {
   render() {
     const projectContent = this.state.projectDetails;
     const { id } = this.props.match.params;
-    // console.log(this.state.projectDetails.actions[0]);
 
     if (this.state.projectDetails.actions !== undefined) {
       console.log(this.state.projectDetails.actions[0]);
       return (
         <div className="projectName">
           <h1>Project: </h1>
-          <div>Name: {projectContent.name}</div>
+          <h3>Name: {projectContent.name}</h3>
           <div className="projectDetailList">
-            <div>Description: {projectContent.description}</div>
-            <div>Completed: {projectContent.completed ? "Yes" : "No"}</div>
-            <div>
+            <h3>Description: {projectContent.description}</h3>
+            <h3>Completed: {projectContent.completed ? "Yes" : "No"}</h3>
+            <h3>
               Actions:
               <div>
                 <ul>
@@ -53,16 +52,19 @@ export default class ProjectDetail extends Component {
                         eachAction.id
                       }/`}
                     >
-                      <h3>Action Description: {eachAction.description}</h3>
-                      <h3>Action Notes: {eachAction.notes}</h3>
-                      <h3>
-                        Action completed: {eachAction.completed ? "Yes" : "No"}
-                      </h3>
+                      <div className="ActionCard">
+                        <h6>Action Description: {eachAction.description}</h6>
+                        <h6>Action Notes: {eachAction.notes}</h6>
+                        <h6>
+                          Action completed:{" "}
+                          {eachAction.completed ? "Yes" : "No"}
+                        </h6>
+                      </div>
                     </Link>
                   ))}
                 </ul>
               </div>
-            </div>
+            </h3>
           </div>
         </div>
       );
